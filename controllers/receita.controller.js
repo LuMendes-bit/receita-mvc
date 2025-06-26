3dd4b470c9815a76c54a1f3ec575858329810f72
Lucas Mendes <lucas.mendes.1234@hotmail.com>
2025-06-25 22:12:16 -0300

    receitas mvc

--- controllers/receita.controller.js
+++ controllers/receita.controller.js
@@ -4,8 +4,6 @@ function listarReceitas(request, response) {
 
     const receitas = receitaModel.listarReceitas()
 
-     //response.json(livros)
-
     response.render('listar-receitas', {
       receitas
     })
